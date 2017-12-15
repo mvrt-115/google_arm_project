fb6a23c713dafd39118fac78abda0435af03bdf3
andwhyd <33667699+andwhyd@users.noreply.github.com>
2017-12-14 19:36:30 -0800

    Added comments and more methods

--- Raspberry PI Code  (Will Run on RPI Only)/TTTArm.py
+++ Raspberry PI Code  (Will Run on RPI Only)/TTTArm.py
@@ -2,14 +2,52 @@
 import random
 import serial
 
-class Board:
-    board = []
+class Arm:
+    def __init__(self, port):
+        try:
+            self.ser = serial.Serial(port, 115200) # Open serial line
+            while self.ser.read_until('$').decode('utf-8') != 'ready':
+                self.ser.write(b'N$')
+        except  serial.serialutil.SerialException:
+            print('Failed to open port: ' + port)
+            exit()
+    
+    def isBusy(self):
+        # Checkes if the Arduino is busy.
+        self.ser.write(b'Busy$')
+        if self.ser.read_until('$').decode('utf-8') == 'busy':
+            return True
+        return False
+
+    # Communication with Arduino
+
+    def movePos(self, letter, move):
+        # Sends a signal to the arm to draw the input letter at the input position.
+        if self.isBusy() != True:
+            self.ser.write(b('P'+str(letter)+str(move)+'$'))
+            if self.ser.read_until('$').decode('utf-8') == 'Invalid':
+                print('Falied: Invalid request')
+                return False
+            else:
+                print('Success: ' + str(letter) + ' placed at ' + str(move))
+                return True
+        print('Falied: Arduino is Busy')
+        return False
+
+    # Optional commands can be added
+
+    def close(self):
+        # Closes the serial port.
+        self.ser.close()
+        print('Serial port closed')
+
+class Board: # Should be complete
     def __init__(self):
         self.board = [0]*10
 
     def draw(self):
-        # This function prints out the board that it was passed.
-        # "board" is a list of 10 strings representing the board (ignore index 0)
+        # Prints out the board in current state.
+        # "board" is a list of 10 ints representing the board (ignore index 0).
         print('   |   |' + '         |   |')
         print(' ' + str(self.board[1]) + ' | ' + str(self.board[2]) + ' | ' + str(self.board[3]) + '     1 | 2 | 3')
         print('   |   |' + '         |   |')
@@ -23,6 +61,7 @@ class Board:
         print('   |   |' + '         |   |')
 
     def makeMove(self, letter, move):
+        # Makes a move on the board based on the input letter and position.
         if self.board[move] == 0:
             self.board[move] = letter
             return True
@@ -31,8 +70,8 @@ class Board:
             return False
 
     def isWinner(self, le):
-        # Given a board and a player's letter, this function returns True if that player has won.
-        # We use bo instead of board and le instead of letter so we don't have to type as much.
+        # Given a board and a letter, this function returns True if that letter has won.
+        # Using bo instead of board and le instead of letter.
         return ((self.board[7] == le and self.board[8] == le and self.board[9] == le) or # across the top
         (self.board[4] == le and self.board[5] == le and self.board[6] == le) or # across the middle
         (self.board[1] == le and self.board[2] == le and self.board[3] == le) or # across the bottom
@@ -49,56 +88,60 @@ class Board:
                 return False
         return True
 
-class Arm:
-    def __init__(self):
-        self.ser = serial.Serial('/dev/ttyACM0', 115200)
-    # communication stuff
-
-    def move(self, letter, move):
-        # stuff
-
-
 class TTTGame:
     def __init__(self):
         self.board = Board()
-        self.diff = 0 # 0 = easiest, 1 = normal, 2 = impossible
-        self.letter = 0 # 0 = blank, 1 = X, 2 = O
-        self.arm = Arm()
+        self.playerLetter = 0 # 0 = blank, 1 = X, 2 = O
+        # self.arm = Arm('/dev/ttyACM0')
 
     def draw(self):
+        # Prints the board, mainly used for debugging.
         self.board.draw()
 
+    def whichTurn(self,letter):
+        # Determines the letter for the current turn.
+        if letter == 1:
+            return 2
+        return 1
+
+
     def makeMove(self, letter, move):
+        # Moves the letter (determined by whichTurn()) to the imput move.
+        # Upon being verified, sends a signal for the arm to physically draw the letter at the postion.
         if self.board.makeMove(letter, move):
-            print('Success: ' + str(letter) + ' placed at ' + str(move))
-            # self.arm(stuff,stuff,stuff)
-        else: print('Failed to place ' + str(letter) + ' at ' + str(move))
+            print('Ok: ' + str(letter) + ' can be placed at ' + str(move))
+            # self.arm.movePos(letter,move)
+        else:
+            print('Error: ' + str(letter) + ' cannot be placed at ' + str(move))
 
     def isWinner(self, le):
+        # Checks if the given letter has won.
         if self.board.isWinner(le):
             return True
         return False
 
     def isBoardFull(self):
+        # Checks if the board is full.
         if self.board.isBoardFull():
             return True
         return False
 
-    def setDiff(difficulty):
-        self.diff = difficulty
-    def getDiff(self):
-        return self.diff
-
-    def setPLetter(le):
-        self.letter = le
+    def setPLetter(self, le):
+        self.playerLetter = le
     def getPLetter(self):
-        return self.letter
+        return self.playerLetter
 
     def getComputerMove(self):
-        print('i cant code')
+        # Determines the optimal move for the computer.
+        print('i cant code') # use logic from ttt test code
 
-    # more stuff
+    # Game logic and more need to be added
+    # Not sure how to have the game "run", but still take input from pushtotalk program
+    # Game Logic may have to be implemented in pushtotalk program
 
+    def quit(self):
+        # self.arm.close()
+        exit()
 
 
 #Testing:
@@ -111,3 +154,4 @@ g.makeMove(1,9)
 g.draw()
 print('X is winner: ' + str(g.isWinner(1)))
 print('Is board full: ' + str(g.isBoardFull()))
+g.quit()
