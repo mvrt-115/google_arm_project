78eb74f55921be21d71b4cd302507470269b2658
Gautham G <gauthamgujjula@gmail.com>
2017-12-24 22:25:53 -0800

    Added all commands, but not responses.

--- Arduino Code (Arm Code)/FullSerial/FullSerial.ino
+++ Arduino Code (Arm Code)/FullSerial/FullSerial.ino
@@ -24,6 +24,8 @@
 #define JOINT_2_OFFSET  -3650 //in steps
 #define ELEVATOR_OFFSET -16000 //in steps
 
+boolean commandReady = false; //boolean to determine if start-up was successful.
+
 AccelStepper joint1(1, 67, 66); // 1, stp, dir
 AccelStepper joint2(1, 65, 64); //1, stp, dir
 AccelStepper elevator(1, 69, 68); //1, stp, dir
@@ -47,14 +49,21 @@ boolean xTurn = true; // Used to track turns
 
 void setup() {
   Serial.begin(BAUD_RATE);
+  while(!Serial){
+    ;
+  }
   pinMode(13,OUTPUT);
   digitalWrite(13,LOW);
+  commandReady = true;
+  Serial.println("Ready");
+}
+
+boolean newGame(){
   joint1.setAcceleration(JOINT_1_ACCELERATION);
   joint2.setAcceleration(JOINT_2_ACCELERATION);
   elevator.setAcceleration(ELEVATOR_ACCELERATION);
   elevator.setMaxSpeed(ELEVATOR_MAX_SPEED);
   offset();
-  Serial.println("Start");
 }
 
 void offset() {
@@ -77,20 +86,52 @@ void loop() {
 }
 
 void serialEvent() {
-  if (Serial.available()) {
-    int inputInt = Serial.read() - '0';
-    Serial.println("recieved:");
-    Serial.println(coords[inputInt*2-2]);
-    Serial.println(coords[inputInt*2-1]);
-    if (inputInt == 0)
-      xTurn = true;
-    else {
-      if (xTurn)
-        drawX(coords[inputInt*2-2], coords[inputInt*2-1]);
-      else
-        drawCircle(coords[inputInt*2-2], coords[inputInt*2-1]);
-      xTurn = !xTurn;
+  String incomingCommand = String();
+  char nextChar = '$';
+  while (Serial.available()) {
+    nextChar = Serial.read();
+    Serial.println(nextChar);
+    incomingCommand.concat(nextChar);
+    Serial.println(incomingCommand);
+  }
+  if(incomingCommand.charAt(0) == 'N'){
+    newGame();
+  }
+  else if(incomingCommand.charAt(0) == 'P'){
+    int toMark = incomingCommand.charAt(4) - '0';
+    if(toMark < 1 || toMark > 9){
+      Serial.println("Invalid");
     }
+    if(incomingCommand.charAt(2) == 'X'){
+      drawX(coords[toMark*2-2], coords[toMark*2-1]);
+    }
+    else if(incomingCommand.charAt(2) == 'O'){
+      drawCircle(coords[toMark*2-2], coords[toMark*2-1]);
+    }
+    else{
+      Serial.println("Invalid");
+    }
+  }
+  else if(incomingCommand.charAt(0) == 'C'){
+    positionsStr = incomingCommand.substring(4);
+    firstPosition = positionsStr.substring(0,positionsStr.indexOf(' '));
+    secondPosition = positionsStr.substring(positionsStr.indexOf(' ') + 1);
+    double x = firstPosition.toDouble();
+    double y = secondPosition.toDouble();
+    if(incomingCommand.charAt(2) == 'X'){
+      drawX(x, y);
+    }
+    else if(incomingCommand.charAt(2) == 'O'){
+      drawCircle(x, y);
+    }
+    else{
+      Serial.println("Invalid");
+  }
+  else if(incomingCommand.charAt(0) == 'W'){
+
+  }
+  else{
+    Serial.println("Invalid");
   }
 }
 
